fd1021f2b6771ba5a0e4ce656bff82170012772f
w.cieslak <w.cieslak@samsung.com>
2018-12-18 16:04:47 +0100

    Local extremes calac - trend line, support&residences group

--- main.js
+++ main.js
@@ -26,14 +26,14 @@ function calcExtremes(data) {
         min: [],
         max: []
     };
-    for (index = 2; index < 498; index++) {
-        if ((data[index].avg > data[index + 2].avg) && (data[index].avg > data[index + 1].avg)) {
-            if ((data[index].avg > data[index - 2].avg) && (data[index].avg > data[index - 1].avg)) {
+    for (index = 3; index < 497; index++) {
+        if ((data[index].avg > data[index + 3].avg) && (data[index].avg > data[index + 2].avg) && (data[index].avg > data[index + 1].avg)) {
+            if ((data[index].avg > data[index - 3].avg) && (data[index].avg > data[index - 2].avg) && (data[index].avg > data[index - 1].avg)) {
                 extremes.max.push({ max: data[index].avg, timestamp: data[index].timestamp })
             }
         }
-        if ((data[index].avg < data[index + 2].avg) && (data[index].avg < data[index + 1].avg)) {
-            if ((data[index].avg < data[index - 2].avg) && (data[index].avg < data[index - 1].avg)) {
+        if ((data[index].avg < data[index + 3].avg) && (data[index].avg < data[index + 2].avg) && (data[index].avg < data[index + 1].avg)) {
+            if ((data[index].avg < data[index - 3].avg) && (data[index].avg < data[index - 2].avg) && (data[index].avg < data[index - 1].avg)) {
                 extremes.min.push({ min: data[index].avg, timestamp: data[index].timestamp })
             }
         }
@@ -50,46 +50,64 @@ const leveragePath = '/api/v1/position/leverage';
 const tradePath = '/api/v1/trade';
 const tradeBucketedPath = tradePath + '/bucketed';
 
-function supres(data, ext) {
-    if (data.length < 1) {
-        return null
+function groupSupres(data) {
+    let result = data.sort().reduce((r, n) => {
+        let lastSubArray = r[r.length - 1];
+
+        if (!lastSubArray || (lastSubArray[lastSubArray.length - 1] * 1.03 < n || lastSubArray[lastSubArray.length - 1] * 0.97 > n)) {
+            r.push([]);
+        }
+        r[r.length - 1].push(n);
+        return r;
+    }, []);
+    return result.map( element => {
+        return {
+            price: Math.floor(element.reduce( (a, b) => a+b)/element.length),
+            quantity: element.length
+        }            
+    }).sort((a,b) => a.price - b.price);
+}
+
+
+
+function checkSupRes(currenPrice, trend1m, sup1d, sup5m, sup1m, res1d, res5m, res1m) {
+    let s1d = sup1d.filter( element => element.price < currenPrice).sort((a,b) => b.price-a.price)[0];
+    let s5m = sup5m.filter( element => element.price < currenPrice).sort((a,b) => b.price-a.price)[0];
+    let s1m = sup1m.filter( element => element.price < currenPrice).sort((a,b) => b.price-a.price)[0];
+    let r1d = res1d.filter( element => element.price > currenPrice).sort((a,b) => a.price-b.price)[0];
+    let r5m = res5m.filter( element => element.price > currenPrice).sort((a,b) => a.price-b.price)[0];
+    let r1m = res1m.filter( element => element.price > currenPrice).sort((a,b) => a.price-b.price)[0];
+    console.log(trend1m,currenPrice,"RESISTANCE",r1d,r5m,r1m,"SUPPORT",s1d,s5m,s1m)
+    if(trend1m < 0) {
+        
     }
-    else {
-        let ret = data.filter( element => {
-            return (data[0][ext]*1.05>element[ext])&&(data[0][ext]*0.95<element[ext])
-        })
-        let rest = data.filter( element => {
-            return (data[0][ext]*1.05<element[ext])&&(data[0][ext]*0.95>element[ext])
-        })
-        console.log(ret,rest)
-        return ret.push(supres(rest))
+    if(trend1m > 0) {
+
     }
 }
 
-function checkCurrentStatus() {
+function checkCurrentStatus(currentPrice) {
     let data = null;
     data = { symbol: "XBTUSD", binSize: "1d", count: 500, reverse: true };
     bitmex(get, tradeBucketedPath, data).then((result) => {
         var ohlcData1d = result.reverse().map(element => element = new ohlcDataModel(element));
         var extremes1d = calcExtremes(ohlcData1d);
-        console.log(supres(extremes1d.min, 'min'));
-        console.log(checkTrend(ohlcData1d, 100));
-        console.log(checkTrend(ohlcData1d, 25));
-        console.log(checkTrend(ohlcData1d, 5));
+        var support1d = groupSupres(extremes1d.min.map(item => item.min));
+        var resistance1d = groupSupres(extremes1d.max.map(item => item.max));
         data = { symbol: "XBTUSD", binSize: "5m", count: 500, reverse: true };
         bitmex(get, tradeBucketedPath, data).then((result) => {
             var ohlcData5m = result.reverse().map(element => element = new ohlcDataModel(element));
             var extremes5m = calcExtremes(ohlcData5m);
-            console.log(checkTrend(ohlcData5m, 100));
-            console.log(checkTrend(ohlcData5m, 25));
-            console.log(checkTrend(ohlcData5m, 5));
+            var support5m = groupSupres(extremes5m.min.map(item => item.min));
+            var resistance5m = groupSupres(extremes5m.max.map(item => item.max));
             data = { symbol: "XBTUSD", binSize: "1m", count: 500, reverse: true };
             bitmex(get, tradeBucketedPath, data).then((result) => {
                 var ohlcData1m = result.reverse().map(element => element = new ohlcDataModel(element));
                 var extremes1m = calcExtremes(ohlcData1m);
-                console.log(checkTrend(ohlcData1m, 100));
-                console.log(checkTrend(ohlcData1m, 25));
-                console.log(checkTrend(ohlcData1m, 5));
+                var support1m = groupSupres(extremes1m.min.map(item => item.min));
+                var resistance1m = groupSupres(extremes1m.max.map(item => item.max));
+                var trend1m = checkTrend(ohlcData1m, 5);
+                checkSupRes(currentPrice, trend1m ,support1d, support5m, support1m, resistance1d, resistance5m, resistance1m);
             });
         });
     });
@@ -130,8 +148,7 @@ client.on('connect', function (connection) {
     });
     connection.on('message', function (message) {
         if (JSON.parse(message.utf8Data).data) {
-            console.log(JSON.parse(message.utf8Data).data);
-            checkCurrentStatus();
+            checkCurrentStatus(JSON.parse(message.utf8Data).data[0].close);
         }
     });
 });
