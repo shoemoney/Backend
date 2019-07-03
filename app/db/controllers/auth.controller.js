143a38619405dc91511e015835fb05fc5525ccf4
w.cieslak <w.cieslak@samsung.com>
2019-07-03 15:17:10 +0200

    Code refactoring

--- app/db/controllers/auth.controller.js
+++ app/db/controllers/auth.controller.js
@@ -9,6 +9,8 @@ exports.checkUser = function (req, res) {
                 if (isMatch) {
                     let username = req.body.username;
                     jwt.sign({ username }, 'secretkey', { expiresIn: '10min' }, (err, token) => {
+                        element = element.toObject();
+                        delete element.password;
                         res.json(
                             { token, user: element }
                         )
@@ -23,3 +25,20 @@ exports.checkUser = function (req, res) {
         }
     })
 };
+
+exports.verifyToken = function (req, res, next) {
+    const bearerHeader = req.headers['authorization']
+    if (typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(' ')[1];
+        req.token = bearerToken;
+        jwt.verify(req.token, 'secretkey', (err, data) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                next()
+            }
+        })
+    } else {
+        res.sendStatus(403);
+    }
+}
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,29 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const user_controller = require('../db/controllers/user.controller');
+const auth = require('../db/controllers/auth.controller')
 
 router.get('/', user_controller.getAll);
-router.post('/create', verifyToken, user_controller.create);
+router.post('/create', auth.verifyToken, user_controller.create);
 router.get('/:id', user_controller.details);
 router.put('/:id/update', user_controller.update);
 router.delete('/:id/delete', user_controller.delete);
-
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization']
-    if (typeof bearerHeader !== 'undefined') {
-        const bearerToken = bearerHeader.split(' ')[1];
-        req.token = bearerToken;
-        jwt.verify(req.token, 'secretkey', (err, data) => {
-            if (err) {
-                res.sendStatus(403);
-            } else {
-                next()
-            }
-        })
-    } else {
-        res.sendStatus(403);
-    }
-}
 
 module.exports = router;
